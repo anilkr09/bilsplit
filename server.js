bb1aab83178ed26962c83ad1cd150a7943274fb6
anilkr09 <69214693+anilkr09@users.noreply.github.com>
2021-10-08 20:10:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+if(process.env.NODE_ENV !== 'production'){
+	require('dotenv').config()
+}
+const express = require('express');
+const app = express();
+// const bcrypt = require('bcrypt');
+const passport =  require('passport')
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+const methodOverride = require('method-override')
+const flash = require('express-flash')
+app.use(flash());
+const session =  require('express-session')
+//passport-config use np
+var initialize = require('./passport-config');
+initialize();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}))
+
+
+app.use(session({
+secret : process.env.SESSION_SECRET,
+resave :false,
+saveUninitialized:false
+}	
+))
+
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+var catalogRouter = require('./routes/catalog');
+
+app.use('/', catalogRouter);
+
+app.listen('5000');
+app.set('view engine','ejs');
+
+
+// app.get('/login',(req,res)=>{res.render('login');})
+
+module.exports =  app;
